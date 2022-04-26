db907127980ad1ecbe5e5e7d891f6507c0c36cd5
jordanm88 <71640595+jordanm88@users.noreply.github.com>
2022-04-26 16:37:25 +0100

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const colors = ['#6D5D4B', '#3D8FE8', '#EF9636', '#4FC4A5', '#000']
+export const colors = ['#6222CC', '#3D8FE8', '#EF9636', '#4FC4A5', '#000']
 export const fonts = [
   'Poppins',
   'sans-serif',
